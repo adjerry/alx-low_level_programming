2d4ea5086b1ab74706fe2485f1da6d305e4e4162
adjerry <adjerusa@gmail.com>
2022-09-11 08:44:05 -0700

    print_alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+char lowercase;
+for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
+{
+if (lowecase == 'q')
+continue;
+if (lowecase == 'e')
+continue;
+putchar(lowercase);
+}
+putchar('\n');
+return (0);
+}
