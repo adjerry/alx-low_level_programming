37ef1b1f022feeaed5194aa5dda26263beb0fc18
adjerry <adjerusa@gmail.com>
2022-09-11 07:59:55 -0700

    print_alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,10 +7,10 @@
 int main(void)
 {
 char alphabet;
-for(alphabet = 'a'; alphabet <= 'z'; alphabet++)
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 {
 putchar(alphabet);
 }
-putchar('\n');
+putchar('\n')
 return (0);
 }
