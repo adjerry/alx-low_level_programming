1684dc60b530f3f8913887f5b804b355e491e9a5
adjerry <adjerusa@gmail.com>
2022-09-13 13:18:55 -0700

    print_alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *main - print alphabet
+ *
+ *Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+char c, 1;
+for (1 = 0; 1 <= 9; 1++)
+{
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
+}
