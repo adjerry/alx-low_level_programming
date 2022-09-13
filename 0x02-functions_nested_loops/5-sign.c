f3c0e3e3696aeea99b8aa57693e12c1ad577cbdb
adjerry <adjerusa@gmail.com>
2022-09-13 15:59:34 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,11 +2,11 @@
 /**
  *print_sign - to cheak sign of the number
  *@n:is the int that will use the argument of the function
- *Return: Always 0.
+ *Return: Always 0
  */
 int print_sign(int n)
 {
-if (n > 0) 
+if (n > 0)
 {
 _putchar('+');
 return (1);
