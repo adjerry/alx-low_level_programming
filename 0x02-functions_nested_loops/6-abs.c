1fde095d3bf5b350bedd8213a130e07202d2873d
adjerry <adjerusa@gmail.com>
2022-09-13 22:12:45 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *_abs - function that computes the absolute value of an integer.
+ *@c: is the int use for argument of the function
+ *Return: Always 0
+ */
+int main(void)
+{
+if (c > 0 || c == 0)
+{
+return (0);
+}
+else
+{
+return (c * -1);
+}
+}
