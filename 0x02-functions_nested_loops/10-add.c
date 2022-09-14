286c2c3141138be7bf6b89b089bd6b06b7598631
adjerry <adjerusa@gmail.com>
2022-09-13 23:59:32 -0700

    add

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,6 +3,7 @@
  *add - a function that adds two integers
  *@num1 - the first intger to be added.
  *@num2 - the second integer to be added.
+ *
  *Return: Always 0.
  */
 int add(int num1, int num2)
