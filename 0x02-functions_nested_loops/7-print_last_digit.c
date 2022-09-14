51385ecf8e5ae4bcabff1fceef5f79314d817238
adjerry <adjerusa@gmail.com>
2022-09-13 23:07:53 -0700

    last_digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,9 +8,7 @@ int print_last_digit(int c)
 {
 int last_digit = c % 10;
 if (last_digit < 0)
-{
 last_digit *= -1;
- _putchar(last_digit + '0');
+_putchar(last_digit + '0');
 return (last_digit);
-}
 }
