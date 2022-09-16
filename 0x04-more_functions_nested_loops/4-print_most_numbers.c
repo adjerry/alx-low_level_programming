fbe9e00f359506240de9da283ac00f7d248a8886
adjerry <adjerusa@gmail.com>
2022-09-16 06:26:06 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+ /**
+  * print_most_numbers - function that prints the numbers
+  * return nothing return
+  */
+
+void print_most_numbers(void)
+{
+int n;
+for (n = 0; n < 58; n++)
+{
+if ((n == 50) || (n == 52))
+{
+continue;
+}
+putchar(n);
+}
+putchar(10);
+}
