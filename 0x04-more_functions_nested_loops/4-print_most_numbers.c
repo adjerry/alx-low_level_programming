79fe4db338a99148750d0d5bb30f4aced02f495c
adjerry <adjerusa@gmail.com>
2022-09-16 06:49:24 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include "main.h"
  /**
-  *print_most_numbers - function that prints the numbers
+  * print_most_numbers - function that prints the numbers
   *
-  *return: nothing return
+  * return: nothing return
   */
 void print_most_numbers(void)
 {
