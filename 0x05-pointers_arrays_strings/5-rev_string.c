df32448dfdd61d23e2e29c1153067b3d482fcbf0
adjerry <adjerusa@gmail.com>
2022-09-19 12:24:51 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,12 +7,16 @@
  */
 void rev_string(char *s)
 {
-int i = 0;
-while (*(s + i) != '\0')
+char rev = s[0];
+int counter = 0;
+int i;
+while (s[counter] != '\0')
+	counter++;
+for (i =0; i < counter; i++)
 {
-if (i % 2 == 0)
-putchar(*(s + i));
-i++;
+	counter--;
+	rev = s[i];
+	s[i] = s[counter];
+	s[counter] = rev;
 }
-putchar(10);
 }
