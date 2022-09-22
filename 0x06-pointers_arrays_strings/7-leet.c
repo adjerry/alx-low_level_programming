e238a0e3b0c20ccdbaaca98caf16cc600d58158a
adjerry <adjerusa@gmail.com>
2022-09-22 13:17:47 -0700

    leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ *leet - function that encodes a string into 1337
+ *@str:string to be encoded
+ *Return: addrese of the encoded string
+ */
+char *leet(char *str)
+{
+int i = 0;
+while (str[i] != '\0')
+{
+(str[i] = transform(str[i]);
+i++;
+}
+return (str);
+}
+/**
+ *transform - heleper function to map a letter
+ *@x:char to be encoded
+ *Return: the encoded char
+ */
+char transform(char x)
+{
+char mapping_low[8] = {'0', '1', '\0', 'e', 'a', '\0', '\0', 't'};
+char mapping_upper[8] = {'0', '1', '\0', 'E', 'A', '\0', '\0', 'T'};
+int i = 0;
+char replacement = x;
+while (i < 8)
+{
+if (x == mapping_low[i] || x == mapping_upper[i])
+{
+replacement = i + '0';
+break;
+}
+i++;
+}
+return (replacement);
+}
