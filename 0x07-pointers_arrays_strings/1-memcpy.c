abff84bb1e507112a7badd065f098417ec33be02
adjerry <adjerusa@gmail.com>
2022-09-26 09:43:27 -0700

    unsigned int

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,1 +1,18 @@
 #include "main.h"
+/**
+ *_memcpy -  function that copies memory area
+ *@dest:memory area to be copied to
+ *@src:memory area to be copied from
+ *@n:number of byets to be copied
+ *
+ *Return:pointer to the copied memory
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int 1 = 0;
+for (; i < n; i++)
+{
+dest[i] = src[i];
+}
+return (dest);
+}
