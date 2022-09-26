15a48099e71896d72b9a0b753838a1d8e69fcf67
adjerry <adjerusa@gmail.com>
2022-09-26 14:34:43 -0700

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,12 +10,12 @@ char *_strstr(char *haystack, char *needle)
 unsigned int i = 0, j = 0;
 while (haystack[i])
 {
-	while (needle[j] && (haystack[i] == needle[0]))
+while (needle[j] && (haystack[i] == needle[0]))
 {
 if (haystack[i + j] == needle[j])
-	j++;
+j++;
 else
-	break;
+break;
 }
 if (needle[j])
 {
