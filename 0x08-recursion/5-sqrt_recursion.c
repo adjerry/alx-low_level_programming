8d1e78d387653dd8908a153a9276453d1a8036d5
adjerry <adjerusa@gmail.com>
2022-09-28 12:05:24 -0700

    sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *_sqrt_recursion - evaluat function sqt
+ *@i:integer
+ *@n:integer
+ *Return:evaluat sqrt
+ */
+int _sqrt_recursion(int n)
+{
+if (n == 0 || n == 1)
+return (n);
+else if (i * i == n)
+return (i);
+return (-1);
+return (-1);
+}
