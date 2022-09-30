096188f383869c433cdb7433a4b8316b59d4b148
adjerry <adjerusa@gmail.com>
2022-09-30 11:23:27 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *main - print the name of the program
+ *@argc: count arguments
+ *@argv: arguments
+ *Return:always zero
+ */
+int main(int argc, char *argv[])
+{
+int count = 0;
+if (argc > o)
+{
+while (count < argc)
+{
+printf("%s\n", argv[count]);
+count++;
+}
+}
+return (0);
+}
